a5f66d53d3ede98b4faebe2f31c23d6379835d62
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2022-07-07 17:19:25 +0800

    Added staked bnb implementations

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -1,18 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.13;
 
 import './Ownable.sol';
-import './IWBNB.sol';
 import './IERC20.sol';
 
 contract Vault is Ownable {
     uint public duration = 1 minutes;
-    address public wbnbAddress = 0xd9145CCE52D386f254917e481eB44e9943F39138; // local
-    // 0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd; testnet wbnb 
-    IWBNB public wbnb; 
 
+    // Vault shares
     uint public totalSupply;
     mapping(address => uint) public balanceOf;
+
+    // staked token
+    uint public stakedTotalSupply;
+    mapping(address => uint) public stakedBalanceOf;
 
     struct Withdrawal {
         uint id;
@@ -30,12 +31,8 @@ contract Vault is Ownable {
     event PendingWithdrawal(address indexed user, uint amount);
     event Withdrawn(address indexed user, uint withdrawalID);
 
-    constructor() {
-        wbnb = IWBNB(wbnbAddress);
-    }
-
-    function checkWBNBBalance() external view returns(uint) {
-        return wbnb.balanceOf(address(this));
+    function checkBalance() external view returns(uint) {
+        return address(this).balance;
     }
 
     function _mint(address _to, uint _shares) private {
@@ -48,7 +45,7 @@ contract Vault is Ownable {
         balanceOf[_from] -= _shares;
     }
 
-    receive() external payable {} // Helps in unwrapping wbnb
+    receive() external payable {}
 
     function deposit() public payable {
         require(msg.value > 0, "Amount > 0");
@@ -67,58 +64,67 @@ contract Vault is Ownable {
         if (totalSupply == 0) {
             shares = msg.value;
         } else {
-            shares = (msg.value * totalSupply) / wbnb.balanceOf(address(this));
+            shares = (msg.value * totalSupply) / stakedTotalSupply;
         }
 
         _mint(msg.sender, shares);
-        // Send to convert to wbnb
-        (bool success, ) = payable(wbnbAddress).call{value: msg.value}("");
-        require(success, "WBNB convert failed");
+
+        // Register staked bnb
+        stakedTotalSupply += msg.value;
+        stakedBalanceOf[msg.sender] += msg.value;
+
         emit Deposit(msg.sender, msg.value);
     }
 
-    // function withdrawSubmit(uint _shares) external {
-    //     require(_shares > 0, "Shares > 0");
-    //     require(_shares < balanceOf[msg.sender], "Cannot redeem more than you own");
-    //     /*
-    //         a = amount
-    //         B = balance of token before withdraw
-    //         T = total supply
-    //         s = shares to burn
+    function submitWithdrawal(uint _shares) external {
+        require(_shares > 0, "Shares > 0");
+        require(_shares <= balanceOf[msg.sender], "Cannot redeem more than you own");
+        /*
+            a = amount
+            B = balance of token before withdraw
+            T = total supply
+            s = shares to burn
 
-    //         (T - s) / T = (B - a) / B 
+            (T - s) / T = (B - a) / B 
 
-    //         a = sB / T
-    //     */
-    //     uint amount = (_shares * wbnb.balanceOf(address(this))) / totalSupply;
+            a = sB / T
+        */
+        uint amount = (_shares * stakedTotalSupply) / totalSupply;
+        require(amount <= stakedBalanceOf[msg.sender], 'Redeemed tokens more than owned');
 
-    //     // burn the shares 
-    //     _burn(msg.sender, _shares);
+        // burn the shares 
+        _burn(msg.sender, _shares);
+
+        // Remove staked tokens
+        stakedTotalSupply -= amount;
+        stakedBalanceOf[msg.sender] -= amount;
         
-    //     // Set withdrawal with timelock
-    //     withdrawals.push( Withdrawal({
-    //         id: nextWithdrawalID,
-    //         user: msg.sender,
-    //         shares: _shares,
-    //         amountInTokens: amount,
-    //         end: block.timestamp + duration,
-    //         sent: false
-    //     }));
+        // Set withdrawal with timelock
+        withdrawals.push( Withdrawal({
+            id: nextWithdrawalID,
+            user: msg.sender,
+            shares: _shares,
+            amountInTokens: amount,
+            end: block.timestamp + duration,
+            sent: false
+        }));
 
-    //     nextWithdrawalID += 1; // increment the nextWithdrawalID
-    //     emit PendingWithdrawal(msg.sender, amount);
-    // }
+        nextWithdrawalID += 1; // increment the nextWithdrawalID
+        emit PendingWithdrawal(msg.sender, amount);
+    }
 
-    // function withdraw(uint _id) external {
-    //     Withdrawal memory staker = withdrawals[_id];
-    //     require(staker.user == msg.sender, "Withdrawal must be staker");
-    //     require(staker.sent == false, "Withdraw processed already");
-    //     require(block.timestamp > staker.end, "Timelock is active");
+    function withdraw(uint _id) external {
+        Withdrawal storage staker = withdrawals[_id];
+        require(staker.user == msg.sender, "Withdrawal must be staker");
+        require(staker.sent == false, "Withdraw processed already");
+        require(block.timestamp > staker.end, "Timelock is active");
+        require(staker.amountInTokens <= address(this).balance, 'BNB balance not enough');
 
-    //     staker.sent = true;
-    //     wbnb.transfer(msg.sender, staker.amountInTokens);
-    //     emit Withdrawn(msg.sender, _id);
-    // }
+        staker.sent = true;
+        (bool success, ) = payable(msg.sender).call{value: staker.amountInTokens}("");
+        require(success, 'BNB return failed');
+        emit Withdrawn(msg.sender, _id);
+    }
 
     // Helper functions 
 
@@ -126,19 +132,18 @@ contract Vault is Ownable {
         uint _shares = balanceOf[msg.sender];
         require(_shares > 0, "Shares > 0");
 
-        uint amount = (_shares * wbnb.balanceOf(address(this))) / totalSupply;
+        uint amount = (_shares * stakedTotalSupply) / totalSupply;
         
         // burn the shares 
         _burn(msg.sender, _shares);
         
-        // transfer back wbnb plus convert to bnb
-        wbnb.withdraw(amount);
+        // transfer back bnb
         (bool success, ) = payable(msg.sender).call{value: amount}("");
         require(success, "BNB return failed");
     }
 
-    function withdrawWBNBToOwner() external onlyOwner {
-        uint _transfer = wbnb.balanceOf(address(this));
-        wbnb.transfer(owner(), _transfer);
+    function withdrawBNBToOwner() external onlyOwner {
+        (bool success, ) = payable(owner()).call{ value: address(this).balance}("");
+        require(success, 'Return bnb failed');
     }
 }
