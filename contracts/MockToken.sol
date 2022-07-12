6647334bd0e4c918ed33f3840fc1b8449f50d430
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2022-07-12 17:15:34 +0800

    Create MockToken.sol

---
+++ contracts/MockToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
+
+contract MockToken is ERC20 {
+    constructor() ERC20('MockToken', "MTK") {
+        _mint(msg.sender, 100000);
+    }
+
+    function faucet(uint amount) external {
+        _mint(msg.sender, amount);
+    }
+}
