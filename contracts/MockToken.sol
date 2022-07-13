134e824a8dcd779b81984a7a413e5f9a9f18e2be
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2022-07-13 12:36:15 +0800

    Add import erc20.sol

--- contracts/MockToken.sol
+++ contracts/MockToken.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
 
 contract MockToken is ERC20 {
     constructor() ERC20('MockToken', "MTK") {
