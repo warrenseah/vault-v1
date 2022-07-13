e657ab162ff6024ca42f9f4192df64edc539ca51
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2022-07-13 12:37:02 +0800

    Amend pre-mint tokens

--- contracts/MockToken.sol
+++ contracts/MockToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockToken is ERC20 {
     constructor() ERC20('MockToken', "MTK") {
-        _mint(msg.sender, 100000);
+        _mint(msg.sender, 100000 * 10**18);
     }
 
     function faucet(uint amount) external {
