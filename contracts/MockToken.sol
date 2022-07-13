8d99270ced533c74f7f3d29b29e37d7b90f60bb5
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2022-07-13 14:19:34 +0800

    First initial npx hardhat compile

--- contracts/MockToken.sol
+++ contracts/MockToken.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
