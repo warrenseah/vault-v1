14366173fc269c5771a5cb618ee1d746a29b88de
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2022-08-17 23:01:46 +0800

    Update vault.test4.js
    Completed submitWithdrawal and withdraw.
    Complete referral  comms pass if referrer withdraw

--- test/vault.test4.js
+++ test/vault.test4.js
@@ -11,6 +11,7 @@ describe("Vault Affiliate Test", function () {
     let vault, mockToken, MockToken, mockTokenSign;
     let deployer, wallet1, wallet2, wallet3, wallet4;
     let vaultSign, vaultWallet1, vaultWallet2, vaultWallet4;
+    let yieldTokenAmt;
 
     const statusType = {
         Inactive: 0,
@@ -41,25 +42,25 @@ describe("Vault Affiliate Test", function () {
         vaultWallet2 = vault.connect(wallet2);
         vaultWallet3 = vault.connect(wallet3);
         vaultWallet4 = vault.connect(wallet4);
+
+        await vaultSign.changeStatus(statusType.Active);
+        yieldTokenAmt = await mockToken.balanceOf(deployer.address);
+        // Approve yield tokens for platform to spend
+        await expect(mockTokenSign.approve(vault.address, yieldTokenAmt))
+        .to.emit(mockToken, "Approval")
+        .withArgs(deployer.address, vault.address, yieldTokenAmt);
     }
 
     describe("Deposit", function () {
-        let firstAccount, refereeAccount1, refereeAccount2, refereeAccount3, yieldTokenAmt;
+        let firstAccount, refereeAccount1, refereeAccount2, refereeAccount3;
 
         before(async function () {
             await loadFixture(deployContractsFixture); // refresh states back to initial
-            await vaultSign.changeStatus(statusType.Active);
-            yieldTokenAmt = await mockToken.balanceOf(deployer.address);
-
+            
             // Deposit
             const deposit1 = ethers.utils.parseUnits("1");
             const deposit1WithFee = await vault.amtWithFee(feeType.Entry, deposit1);
             await vaultWallet1.deposit(0, { value: deposit1 });
-
-            // Approve yield tokens for platform to spend
-            await expect(mockTokenSign.approve(vault.address, yieldTokenAmt))
-                .to.emit(mockToken, "Approval")
-                .withArgs(deployer.address, vault.address, yieldTokenAmt);
         });
         it("should update accounts struct", async function() {
             firstAccount = await vault.accounts(wallet1.address);
@@ -217,9 +218,20 @@ describe("Vault Affiliate Test", function () {
             refereeAccount2 = await vault.accounts(wallet3.address);
             let stakeArr3 = await vault.addressToStakeArr(wallet3.address);
             expect(stakeArr3.length).to.equal(2, "user does not have more than 1 stakes");
-
             // submitWithdraw for stake.id 3
             await vaultWallet3.submitWithdrawal(stakeArr3[0].toString());
+            
+            const stakeId3 = await vault.stakes(2);
+            expect(stakeId3.tillTime).to.be.gt(0, "Unstake time is not registered");
+
+            const withdrawArr3 = await vault.addressToWithdrawArr(wallet3.address);
+            expect(withdrawArr3.length).to.equal(1, "withdrawal array does not have items");
+            expect(withdrawArr3[0]).to.equal(1, "withdrawal array does not have items");
+
+            const withdrawId1 = await vault.withdrawals(0); // id 1 - 1 = 0
+            expect(withdrawId1.user).to.equal(wallet3.address, "user is not the same");
+            expect(withdrawId1.end).to.be.gt(0, "endTime must be set for an end to timelock");
+            expect(withdrawId1.sent).to.be.false;
 
             // should decrement by 1 in stake3Arr
             const latestRefereeAccount2 = await vault.accounts(wallet3.address);
@@ -243,6 +255,113 @@ describe("Vault Affiliate Test", function () {
 
             refereeAccount1 = await vault.accounts(wallet2.address);
             expect(refereeAccount1.referredCount).to.equal(0, "referredCount did not decrement");
+        });
+
+        it("should withdraw successfully", async function() {
+            await time.increase(3600);
+            const stakeArr3 = await vault.addressToWithdrawArr(wallet3.address);
+            await expect(vaultWallet3.withdraw(stakeArr3[0].toString())).to.changeEtherBalance(vaultWallet3, "-3960000000000000000").to.emit(vaultWallet3, "Withdrawn").withArgs(wallet3.address, 0); // withdraw id 1 (0)
+
+            const withdrawId1 = await vault.withdrawals(0); // id 1 - 1 = 0
+            expect(withdrawId1.user).to.equal(wallet3.address, "user is not the same");
+            expect(withdrawId1.sent).to.be.true;
+
+            const latestStakeArr3 = await vault.addressToWithdrawArr(wallet3.address);
+            expect(latestStakeArr3.length).to.equal(1, "withdrawal array does not have items");
+            expect(latestStakeArr3[0]).to.equal(2, "withdrawal item is not 2");
+            
+            await expect(vaultWallet3.withdraw(stakeArr3[1].toString())).to.emit(vaultWallet3, "Withdrawn").withArgs(wallet3.address, 1); // withdraw id 2 (1)
+            const withdrawId2 = await vault.withdrawals(1); // id 2 - 1 = 1
+            expect(withdrawId2.user).to.equal(wallet3.address, "user is not the same");
+            expect(withdrawId2.sent).to.be.true;
+
+            const finalStakeArr3 = await vault.addressToWithdrawArr(wallet3.address);
+            expect(finalStakeArr3).to.be.empty;
+        });
+
+        it("should not pay referral if parent.hasStakes is false", async function() {
+            await loadFixture(deployContractsFixture); // refresh states back to initial
+            await vaultWallet1.deposit(0, {value: ethers.utils.parseUnits("1")});
+            await vaultWallet2.deposit(1, {value: ethers.utils.parseUnits("3")});
+            await vaultWallet3.deposit(2, {value: ethers.utils.parseUnits("3")});
+
+            firstAccount = await vault.accounts(wallet1.address);
+            refereeAccount1 = await vault.accounts(wallet2.address);
+            refereeAccount2 = await vault.accounts(wallet3.address);
+
+            // Admin end yield
+            await time.increase(3600); // move time so that deposits can claim token yields later
+            await vaultSign.addYieldTokens(await time.latest(),await vault.totalStakes());
+            await time.increase(3600);
+            await vaultSign.amendYieldTokens(1, mockToken.address, yieldTokenAmt, 0, await time.latest());
+            
+            // refereeAccount1 withdraw
+            await vaultWallet2.submitWithdrawal(2);
+            refereeAccount1 = await vault.accounts(wallet2.address);
+            expect(refereeAccount1.haveStakes).to.be.false;
+            await expect(vaultWallet2.claimYieldTokens(2,1)).to.be.revertedWith("stakeId must belong to caller");
+
+            // refereeAccount 2 claimYieldTokens, directReferrer withdrawn will not get reward, second-level referrer will receive rewards 30%
+            await expect(vaultWallet3.claimYieldTokens(3,1))
+                .to.emit(vaultWallet3, "PaidReferral").withArgs(wallet3.address, wallet1.address, anyValue, 2, mockToken.address)
+                .to.emit(vaultWallet3, "ClaimedTokens").withArgs(0,2,mockToken.address, wallet3.address, anyValue);
+            
+
+            const secondTierRewards = await vault.tokensOfUserBalance(mockToken.address, wallet1.address); // 3%
+            expect(await vault.tokensOfUserBalance(mockToken.address, wallet2.address)).to.equal(0, "direct referrer should not have comms");
+            expect(secondTierRewards).to.be.gt(0, "second-level referrer should have comms");
+
+            const rewards = await mockToken.balanceOf(wallet3.address); // 70%
+            const adminRewards = await vault.profitsInToken(mockToken.address); // 27%
+
+            // console.log(rewards);
+            // console.log(adminRewards);
+            // console.log(secondTierRewards);
+        });
+
+        it("should not pay any referral if all parent.hasStakes are false", async function() {
+            await loadFixture(deployContractsFixture); // refresh states back to initial
+            await vaultWallet1.deposit(0, {value: ethers.utils.parseUnits("1")});
+            await vaultWallet2.deposit(1, {value: ethers.utils.parseUnits("3")});
+            await vaultWallet3.deposit(2, {value: ethers.utils.parseUnits("3")});
+
+            firstAccount = await vault.accounts(wallet1.address);
+            refereeAccount1 = await vault.accounts(wallet2.address);
+            refereeAccount2 = await vault.accounts(wallet3.address);
+
+            // Admin end yield
+            await time.increase(3600); // move time so that deposits can claim token yields later
+            await vaultSign.addYieldTokens(await time.latest(),await vault.totalStakes());
+            await time.increase(3600);
+            await vaultSign.amendYieldTokens(1, mockToken.address, yieldTokenAmt, 0, await time.latest());
+            
+            // firstAccount withdraw
+            await vaultWallet1.submitWithdrawal(1);
+            firstAccount = await vault.accounts(wallet1.address);
+            expect(firstAccount.haveStakes).to.be.false;
+            await expect(vaultWallet1.claimYieldTokens(1,1)).to.be.revertedWith("stakeId must belong to caller");
+
+            // refereeAccount1 withdraw
+            await vaultWallet2.submitWithdrawal(2);
+            refereeAccount1 = await vault.accounts(wallet2.address);
+            expect(refereeAccount1.haveStakes).to.be.false;
+            await expect(vaultWallet2.claimYieldTokens(2,1)).to.be.revertedWith("stakeId must belong to caller");
+
+            // refereeAccount 2 claimYieldTokens, directReferrer withdrawn will not get reward, second-level referrer will receive rewards 30%
+            const [totalRewards, netRewards] = await vaultWallet3.getClaimsFor(3,1);
+            const adminRewards = totalRewards.sub(netRewards);
+            await expect(vaultWallet3.claimYieldTokens(3,1))
+                .to.emit(vaultWallet3, "ClaimedTokens").withArgs(0,2,mockToken.address, wallet3.address, anyValue);
+            
+            expect(await vault.tokensOfUserBalance(mockToken.address, wallet2.address)).to.equal(0, "direct referrer should not have comms");
+            expect(await vault.tokensOfUserBalance(mockToken.address, wallet1.address)).to.equal(0, "second-level referrer should not have comms");
+
+            const rewards = await mockToken.balanceOf(wallet3.address); // 70%
+            const adminProfits = await vault.profitsInToken(mockToken.address); // 30%
+            expect(adminProfits).to.equal(adminRewards, "admin profitInTokens is not the same");
+
+            // console.log(rewards);
+            // console.log(adminProfits);
         });
     });
 });
